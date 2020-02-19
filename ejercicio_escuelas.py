2a5e23b8db0397a573c503c66a21d773bcac7186
David Rivera <51249591+DawffyddRiv@users.noreply.github.com>
2020-02-18 23:40:28 -0600

    Se actualiza agregando la entidad "Horario"
    Se rehízo el concepto agregando a cuatro entidades

--- ejercicio_escuelas.py
+++ ejercicio_escuelas.py
@@ -1,4 +1,4 @@
-#Ojalá y cumpla con los requerimientos mínimos. Saludos colega.
+#Sistema de escuelas por DawffyddRiv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
@@ -7,52 +7,72 @@ from sqlalchemy.orm import sessionmaker, relationship
 engine=create_engine('sqlite:///:memory:')
 Base=declarative_base(engine)
 
-class Student(Base):
-    __tablename__="alumno"
+class Estudiante(Base):
+    __tablename__="alumno"   
     id=Column(Integer,Sequence('alumno_seq_id'),primary_key=True)
     nombrea=Column(String)
+    apellidoa=Column(String)
     curso_ida=Column(Integer,ForeignKey('curso.id'))
-    courses=relationship("Course",back_populates='estudiantes')
+    
+    cursos=relationship("Course",back_populates='estudiantes')
     def __repr__(self):
-        return'{}'.format(self.nombrea)
+        return'{}{}'.format(self.nombrea, self.apellidoa)
 
 class Course(Base):
     __tablename__='curso'
     id=Column(Integer, Sequence('curso_seq_id'),primary_key=True)
     nombrec=Column(String)
-    horario=Column(String)
-    estudiantes=relationship("Student",back_populates='courses')
-    
-    maestros=relationship("Maestro",back_populates='cursillo')
+        
+    estudiantes=relationship("Estudiante",back_populates='cursos')
+    hora_curso=relationship("Crono",back_populates='curso_hora')
     def __repr__(self):
-        return'{}{}'.format(self.nombrec,self.horario)
+        return'{}'.format(self.nombrec)
+
+class Crono(Base):
+    __tablename__='horario'
+    id=Column(Integer, Sequence('horario_seq_id'),primary_key=True)
+    dia=Column(String)
+    hora_inicio=Column(String)
+    hora_fin=Column(String)
+    profesor_id=Column(Integer,ForeignKey('profesor.id'))
+    curso_id=Column(Integer,ForeignKey('curso.id'))
+    
+    curso_hora=relationship("Course",back_populates='hora_curso')
+    curso_profe=relationship("Maestro",back_populates='profe_curso')
+
+    def __repr__(self):
+        return'{}{}{}'.format(self.dia,self.hora_inicio, self.hora_fin)
 
 class Maestro(Base):
     __tablename__='profesor'
     id=Column(Integer, Sequence('profesor_seq_id'),primary_key=True)
     nombrep=Column(String)
-    curso_idp=Column(Integer,ForeignKey('curso.id'))
+    apellidop=Column(String)
     
-    cursillo=relationship("Course",back_populates='maestros')
-
+    profe_curso=relationship("Crono",back_populates='curso_profe')
     def __repr__(self):
-        return'{}'.format(self.nombrep)
+        return'{}{}'.format(self.nombrep, self.apellidop)
+
 
 Base.metadata.create_all(engine)
 
 Session=sessionmaker(bind=engine)
 session=Session()
 
-alumno1=Student(nombrea='Panchito')
+alumno1=Estudiante(nombrea='Panchito', apellidoa='  Gonzalez')
 print(alumno1)
 session.add(alumno1)
-alumno1.courses=Course(nombrec='Quimica',horario='7-8 pm')
-print(alumno1.courses)
+alumno1.cursos=Course(nombrec='Fisica')
+print(alumno1.cursos)
 
-profesor1=Maestro(nombrep='Huidobro')
-session.add(profesor1)
-profesor1.cursillo=Course(nombrec='Quimica ',horario=' 7-8 pm')
-print(profesor1.cursillo)
-print(session.query(Course).filter(Maestro.cursillo.has()).all())
+horario1=Crono(dia='lunes',hora_inicio="7:00 am", hora_fin='8:00')
+session.add(horario1)
+horario1.curso_hora=Course(nombrec='Quimica')
+horario1.curso_profe=Maestro(nombrep='Vicente',apellidop='  Huidobro')
+
+
+print(horario1.curso_profe)
+print(session.query(Course).filter(Maestro.profe_curso.any()).all())
+print(session.query(Crono).filter(Maestro.profe_curso.any()).all())
 
 session.commit()
